39d92bd5fba4b6df06b6aa3192109485b281e727
IamAdinath <adinathgore.17@gmail.com>
2023-08-15 17:22:43 +0530

    update sync file

---
+++ ui/cloudformation/sync.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [ -z "$INSTANCE" ]
+  then
+    echo "Environment name not specified. Please specify a valid Instance from ./instances"
+fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+INSTANCE_PATH=$DIR/instances/${INSTANCE}.sh
+
+if [ -f "$INSTANCE_PATH" ]; then
+    echo "$INSTANCE_PATH exists."
+    . $INSTANCE_PATH
+else
+    echo "$INSTANCE_PATH does not exist."
+    exit 1
+fi
+
+
+function validate_parameters() {
+    local is_error=false
+
+    if [ -z "$INSTANCE" ]; then
+        echo INSTANCE not defined
+        is_error=true
+    fi
+
+    if [ -z "$REGION" ]; then
+        echo REGION not defined
+        is_error=true
+    fi
+
+    if [ -z "$HOSTNAME" ]; then
+        echo HOSTNAME not defined
+        is_error=true
+    fi
+
+    if [ -z "$UI_BUCKET_NAME" ]; then
+        echo UI_BUCKET_NAME not defined
+        is_error=true
+    fi
+
+    if "${is_error}"; then
+        exit 1
+    fi
+}
+
+function deploy() {
+    export REACT_APP_INSTANCE=$INSTANCE
+    # change to ui root directory
+    pushd $DIR/..
+        npm ci
+        npm run build
+    popd
+
+    STACK_NAME=adinath-portfolio-ui-${INSTANCE}
+
+    DISTRIBUTION_ID=$(aws --region ${REGION} cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CloudFrontDistributionID'].OutputValue" --output text)
+    aws --region ${REGION} s3 sync ${DIR}/../build s3://$UI_BUCKET_NAME --acl public-read --delete
+    aws --region ${REGION} cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/' '/*'
+}
+
+
+validate_parameters
+deploy
