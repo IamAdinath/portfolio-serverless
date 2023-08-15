b1c52efb0df50433f66c6daed57c6caacf4d98d3
IamAdinath <adinathgore.17@gmail.com>
2023-08-15 17:26:19 +0530

    update the test instance file

--- ui/cloudformation/instances/test.sh
+++ ui/cloudformation/instances/test.sh
@@ -2,7 +2,7 @@
 # this can be in a file or specified elsewhere in env
 
 REGION=ap-northeast-1
-HOSTNAME=demo-led-mon-v2.test.scorer.jp
+HOSTNAME=adinathgore.com
 ACM_CERTIFICATE_ARN=arn:aws:acm:us-east-1:729700637718:certificate/9ac34336-ac42-478a-9abb-6228cf535553
-UI_BUCKET_NAME=led-monitor-v2-ui-preview
+UI_BUCKET_NAME=-adinath-portfolio-ui
 HOSTED_ZONE_ID=Z26QV6APUHCW5G
