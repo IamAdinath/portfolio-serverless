72d41b9cf9489621fe9f011dc4a2dbe96421ef5c
IamAdinath <adinathgore.17@gmail.com>
2023-12-28 19:48:40 +0530

    add updated App file with links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,32 @@
 // App.tsx
-import React from 'react';
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
-import Portfolio from './Portfolio';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import './App.css'; // Import your global styles
+import Footer from './components/common/Footer'
+import AboutMe from './components/sections/AboutMe';
+import Portfolio from './Portfolio';
+// import Contact from './Contact';
+// import Resume from './Resume';
+
 
 const App: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <ChakraProvider>
-      <CSSReset />
-      <div className="app">
-        <Portfolio />
-      </div>
-    </ChakraProvider>
+    <Router>
+      <Header onMenuClick={handleMenuClick}/>
+      <Routes>
+      <Route path="/" element={<Portfolio />} />
+        <Route path="/about" element={<AboutMe />} />
+        {/* <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
