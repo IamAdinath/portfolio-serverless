bfeb6b81ef865c31e69be360e849fa1e6d216cf9
IamAdinath <adinathgore.17@gmail.com>
2023-12-28 19:49:39 +0530

    add copyrights in footer

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,13 +1,13 @@
 // Footer.tsx
 import React from 'react';
-import { Pane, Link, IconButton } from 'evergreen-ui';
+import { Pane, Link, IconButton, Text } from 'evergreen-ui';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faTwitter, faGoogle, faMedium, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   return (
-    <Pane display="flex" justifyContent="center" alignItems="center" padding={16} background="tint2">
-      <Pane display="flex" alignItems="center">
+    <Pane display="flex" flexDirection="column" justifyContent="center" alignItems="center" padding={16} background="tint2">
+      <Pane display="flex" alignItems="center" marginBottom={8}>
         <Link href="https://twitter.com/prime_whites" target="_blank" marginRight={8}>
           <IconButton icon={<FontAwesomeIcon icon={faTwitter} />} />
         </Link>
@@ -27,6 +27,9 @@ const Footer: React.FC = () => {
           <IconButton icon={<FontAwesomeIcon icon={faStackOverflow} />} />
         </Link>
       </Pane>
+      <Text size={300} color="muted">
+        © {new Date().getFullYear()} Adinath Gore. All rights reserved.
+      </Text>
     </Pane>
   );
 };
