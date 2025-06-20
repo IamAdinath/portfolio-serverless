ead29db04199c1b3d693bde8972c675e25a580a6
IamAdinath <adinathgore.17@gmail.com>
2025-06-20 21:47:37 +0530

    add handler to download resume

---
+++ api/lambda/resume/download.py
@@ -0,0 +1,25 @@
+from common import s3
+from common import utils
+import s3
+from contsants import Headers, StatusCodes
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO').upper())
+
+
+def lambda_handler(event, context):
+    """
+        return s3 file signed url
+    """
+    try:
+        bucket = os.environ['BUCKET']
+        resume_file = os.environ['RESUME_KEY']
+        url = s3.get_s3_file_url(bucket, key)
+        if not url:
+            return utils.build_response(StatusCodes.NOT_FOUND, Headers)
+        return utils.build_response(StatusCodes.OK, Headers, url)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        return utils.build_response(StatusCodes.INTERNAL_SERVER_ERROR, Headers)
