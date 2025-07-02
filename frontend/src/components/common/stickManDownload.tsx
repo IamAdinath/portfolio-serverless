c53f5488674abf2246d40742dc89841bb9241ffd
Adinath Gore <59023488+IamAdinath@users.noreply.github.com>
2025-07-02 14:47:30 +0530

    dev > master : Add download and save resume feature (#38)
    * add rackspace experience (#30)
    
    * fix typo in the resume (#31)
    
    * Feature/add db (#32)
    
    * add db to template
    
    * remove comments
    
    * Feature/add db (#33)
    
    * add db to template
    
    * remove comments
    
    * match the key in schema
    
    * update functions and common utils
    
    * update common utils
    
    * add env variab;e
    
    * add handler and api to craete blog
    
    * add env variable
    
    * update the template
    
    * remove authors table
    
    * add user pools andtable
    
    * add params
    
    * comment the google auth
    
    * update add blog lambda
    
    * add blog domain
    
    * update url modifications
    
    * add new components for auth
    
    * add API file
    
    * add config file for aws
    
    * add lambda to confirm user in cognito
    
    * update template to add lambda
    
    * update api gateway
    
    * update headers
    
    * update username and email places
    
    * add a s3 utils module
    
    * add a s3 bucket
    
    * add new resources to template
    
    * update the build response
    
    * add handler to download resume
    
    * update the Bucket details
    
    * update UI for resume download
    
    * fix indent
    
    * update bucket name
    
    * update bucket policy
    
    * add cors for bucket
    
    * update phone and experience in resume
    
    * update response in utils
    
    * add cors and resume download
    
    * update template
    
    * update template for bucket cors
    
    * update py file reduce error
    
    * remove distribution
    
    * update policy
    
    * remove policy
    
    * update response
    
    * update response headers
    
    * remove unused resources from cfn template
    
    * resolve merge conflicts
    
    * remove remianing confilcts

--- frontend/src/components/common/stickManDownload.tsx
+++ frontend/src/components/common/stickManDownload.tsx
@@ -1,10 +1,7 @@
-import './stickManDownload.css'
-// StickmanDownload.tsx
-import { DownloadResume } from './userAPI';
-
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './stickManDownload.css';
+import { ResumeLink, GetFile } from './userAPI';
 
 const StickmanDownload: React.FC = () => {
   const [isAnimating, setIsAnimating] = useState(false);
@@ -15,21 +12,39 @@ const StickmanDownload: React.FC = () => {
       setIsAnimating(true);
       setShowStickman(true);
 
-      const response = await DownloadResume();
+      const response = await ResumeLink();
       const presignedUrl = response.url;
 
-      setTimeout(() => {
+      setTimeout(async () => {
         if (presignedUrl) {
-          const link = document.createElement('a');
-          link.href = presignedUrl;
-          link.download = 'Adinath_Gore_Resume.pdf';
-          link.click();
+          try {
+            const fileResponse = await GetFile(presignedUrl);
+            if (!fileResponse) throw new Error('Failed to fetch file');
+
+            // const blob = await fileResponse();
+            const blobUrl = window.URL.createObjectURL(fileResponse);
+
+            const link = document.createElement('a');
+            link.href = blobUrl;
+            link.download = 'Adinath_Gore_Resume.pdf';
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+
+            window.URL.revokeObjectURL(blobUrl);
+          } catch (error) {
+            console.error('Error downloading file blob:', error);
+            alert('Failed to download file. Please try again.');
+          }
         }
+
         setIsAnimating(false);
         setShowStickman(false);
       }, 3200); // Sync with animation
+
     } catch (err) {
       console.error('Failed to fetch resume URL:', err);
+      alert('Error getting download link.');
       setIsAnimating(false);
       setShowStickman(false);
     }
