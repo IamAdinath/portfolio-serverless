45727cd5606b2e5e87c7349bfb3d4b1fcb51a4e0
Adinath Gore <59023488+IamAdinath@users.noreply.github.com>
2025-07-12 11:23:34 +0530

    Bug/update blog indexing (#47)
    * remove blogs table
    
    * update create blog handler
    
    * add blogs table
    
    * update env variables
    
    * update put object changes
    
    * update env variable in handler
    
    * reformat handler
    
    * update print > logger

--- api/lambda/blogs/create.py
+++ api/lambda/blogs/create.py
@@ -6,55 +6,77 @@ import boto3
 from common.utils import build_response
 from common.contsants import StatusCodes, Headers
 import logging
-dynamodb = boto3.resource('dynamodb')
-BLOGS_TABLE = os.environ['BLOGS_TABLE']
+
+dynamodb = boto3.resource("dynamodb")
+
 
 logger = logging.getLogger(__name__)
+
+
 def lambda_handler(event, context):
     try:
+        BLOGS_TABLE = os.getenv("BLOGS_TABLE")
+        if not BLOGS_TABLE:
+            return build_response(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                Headers.CORS,
+                {"message": "BLOGS_TABLE env variable not set."},
+            )
         logger.info(f"Received event: {event}")
-        user_id = event['requestContext']['authorizer']['claims']['sub']
+        user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
         if not user_id:
-            return build_response(StatusCodes.UNAUTHORIZED, Headers.CORS, {
-                "message": "User not authenticated."
-            })
+            return build_response(
+                StatusCodes.UNAUTHORIZED,
+                Headers.CORS,
+                {"message": "User not authenticated."},
+            )
         logger.info(f"User ID: {user_id}")
-        body = json.loads(event['body'])
-        title = body.get('title')
-        content = body.get('content')
-        tags = body.get('tags', [])
-        reading_time = body.get('reading_time', 1)  # default 1 min
+        body = json.loads(event["body"])
+        title = body.get("title")
+        content = body.get("content")
+        tags = body.get("tags", [])
+        reading_time = body.get("reading_time", 1)
+        blog_status = body.get("status", "published")
 
         if not title or not content:
-            return build_response(StatusCodes.BAD_REQUEST, Headers.CORS, {
-                "message": "Title and content are required."
-            })
+            logger.error(f"Invalid Title: {title} and or Content: {content}")
+            return build_response(
+                StatusCodes.BAD_REQUEST,
+                Headers.CORS,
+                {"message": "Title and content are required."},
+            )
 
         blog_id = str(uuid.uuid4())
         now = datetime.utcnow().isoformat()
-
         item = {
-            'postId': blog_id,
-            'author_id': user_id,
-            'title': title,
-            'content': content,
-            'tags': tags,
-            'reading_time': reading_time,
-            'images': body.get('images', []),  # expect list of image URLs (S3)
-            'created_at': now,
-            'updated_at': now
+            "id": blog_id,
+            "author": user_id,
+            "title": title,
+            "content": content,
+            "tags": tags,
+            "reading_time": reading_time,
+            "images": body.get("images", []),  # expect list of image URLs (S3)
+            "created_at": now,
+            "updated_at": now,
+            "status": blog_status,
+            "status_published_at": f"{blog_status}_{now}",
+            "author_index": f"{user_id}_{now}",
+            "published_at": now,
         }
 
         table = dynamodb.Table(BLOGS_TABLE)
         table.put_item(Item=item)
 
-        return build_response(StatusCodes.CREATED, Headers.CORS, {
-            "message": "Blog created successfully.",
-            "blog_id": blog_id
-        })
+        return build_response(
+            StatusCodes.CREATED,
+            Headers.CORS,
+            {"message": "Blog created successfully.", "blog_id": blog_id},
+        )
 
     except Exception as e:
-        print("Error:", e)
-        return build_response(StatusCodes.INTERNAL_SERVER_ERROR, Headers.CORS, {
-            "message": "Failed to create blog."
-        })
+        logger.eror(f"Error: {e}")
+        return build_response(
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            Headers.CORS,
+            {"message": "Failed to create blog."},
+        )
