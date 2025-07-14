cfa7af726cc86827696e74241534a0d5249585e4
Adinath Gore <59023488+IamAdinath@users.noreply.github.com>
2025-07-14 23:55:07 +0530

    modify page section (#54)

--- frontend/src/components/sections/Blog.tsx
+++ frontend/src/components/sections/Blog.tsx
@@ -12,8 +12,8 @@ const Blog: React.FC<BlogProps> = ({ title, content, author }) => {
   return (
     <div className="blog-post">
       <h2>{title}</h2>
-      <p>{content}</p>
       <p><em>by {author}</em></p>
+      <p>{content}</p>
     </div>
   );
 };
