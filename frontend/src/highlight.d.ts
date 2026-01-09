2d31b4da1482386afa2fcb1ebeadc4a66565b94d
Adinath Gore <59023488+IamAdinath@users.noreply.github.com>
2026-01-09 23:59:03 +0530

    Delete frontend directory

--- frontend/src/highlight.d.ts
+++
@@ -1,5 +0,0 @@
-// tiptap.d.ts
-// declare module '@tiptap/react';
-// declare module '@tiptap/starter-kit';
-// declare module '@tiptap/extension-placeholder';
-declare module 'highlight.js/lib/languages/*';
